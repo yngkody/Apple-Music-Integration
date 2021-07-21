876f3ac5f1f97b272f78d71be4c371d475e485c3
Teneala Spencer <teneala.spencer@usm.edu>
2021-07-20 22:35:49 -0400

    Initial Commit

---
+++ Apple Music Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Music Code
+//
+//  Created by Kody Young on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
